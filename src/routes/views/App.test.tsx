c8e47e72a4944f3c0f9ce318c85b7f946bb6d67b
triumphvalley <timosiegenthaler@gmail.com>
2022-03-10 09:05:39 +0100

    fix tests

--- src/routes/views/App.test.tsx
+++ src/routes/views/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import RouteTree from '../RouteTree'
 
 test('renders learn react link', () => {
-    render(<App />)
-    const linkElement = screen.getByText(/learn react/i)
+    render(<RouteTree />)
+    const linkElement = screen.getByText('Home')
     expect(linkElement).toBeInTheDocument()
 })
