4aa58c5b58841788fa94fee0b5429a432d0a8690
triumphvalley <timosiegenthaler@gmail.com>
2022-03-10 09:13:56 +0100

    improve test naming

--- src/routes/views/App.test.tsx
+++ src/routes/views/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import RouteTree from '../RouteTree'
 
-test('renders learn react link', () => {
+test('renders «Home» link', () => {
     render(<RouteTree />)
     const linkElement = screen.getByText('Home')
     expect(linkElement).toBeInTheDocument()
