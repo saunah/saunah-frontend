6f0c1d0cfdac5fabac2f67fcde1d249333b4393c
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-03-27 13:24:56 +0200

    add line break on landing page

--- src/routes/views/App.tsx
+++ src/routes/views/App.tsx
@@ -11,6 +11,7 @@ function App() {
             <Outlet />
             <div>
                 <span> REACT_APP_API_BASE_URL: {process.env.REACT_APP_API_BASE_URL || '-'} </span>
+                <br />
                 <span> REACT_APP_TEST_VAR: {process.env.REACT_APP_TEST_VAR || '-'} </span>
             </div>
         </div>
