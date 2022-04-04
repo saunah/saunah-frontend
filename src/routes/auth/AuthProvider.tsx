6fae07f0f80ea14079e1b9b596693c6efbf8381b
triumphvalley <timosiegenthaler@gmail.com>
2022-04-04 18:16:37 +0200

    add protected route test

--- src/routes/auth/AuthProvider.tsx
+++ src/routes/auth/AuthProvider.tsx
@@ -24,7 +24,7 @@ const AuthProvider = (props: AuthProviderProps) => {
             setTimeout(() => {
                 setAuthenticated(true)
                 resolve()
-            }, 500)
+            }, 100)
         })
     }
 
