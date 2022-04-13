beb75b24e5b9ec2094cff97cd17cec01d9a54719
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-04-14 01:07:51 +0200

    add test todo for sonar scan

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,8 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
+// TODO: This is a test for sonar scan
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
