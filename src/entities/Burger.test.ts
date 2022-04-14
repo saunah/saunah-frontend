d508481ffd6a7ea2a18a3fd6782c628518a3289e
triumphvalley <timosiegenthaler@gmail.com>
2022-04-14 15:03:39 +0200

    remove burger

--- src/entities/Burger.test.ts
+++
@@ -1,9 +0,0 @@
-import { Hambuger } from './Burger'
-
-describe('Burger', () => {
-    test('should be correctly priced.', () => {
-        const burger = new Hambuger()
-        burger.addExtras([{ name: 'hello', price: 3 }])
-        expect(burger.getTotalPrice()).toBe(9)
-    })
-})
