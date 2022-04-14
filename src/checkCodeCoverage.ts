8446207a16a4525b6cd69f932153c9ddb9b5ed9f
Simon Schuhmacher <schuhsim@students.zhaw.ch>
2022-04-14 11:28:06 +0200

    add more than 20 lines to make quality gate kick in

--- src/checkCodeCoverage.ts
+++ src/checkCodeCoverage.ts
@@ -1,8 +1,32 @@
 const checkCodeCoverageMethodOne = () => {
     checkCodeCoverageMethodTwo()
+    console.log('Line 1')
+    console.log('Line 2')
+    console.log('Line 3')
+    console.log('Line 4')
+    console.log('Line 5')
+    console.log('Line 6')
+    console.log('Line 7')
+    console.log('Line 8')
+    console.log('Line 9')
+    console.log('Line 10')
+    console.log('Line 11')
+    console.log('Line 12')
 }
 const checkCodeCoverageMethodTwo = () => {
     checkCodeCoverageMethodOne()
+    console.log('Line 1')
+    console.log('Line 2')
+    console.log('Line 3')
+    console.log('Line 4')
+    console.log('Line 5')
+    console.log('Line 6')
+    console.log('Line 7')
+    console.log('Line 8')
+    console.log('Line 9')
+    console.log('Line 10')
+    console.log('Line 11')
+    console.log('Line 12')
 }
 
 export default checkCodeCoverageMethodOne
