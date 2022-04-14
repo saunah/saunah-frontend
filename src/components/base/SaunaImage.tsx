6e3f9ef4eb920d09d5150ad1021df439e813b7c5
inderdav <inderdav@students.zhaw.ch>
2022-04-14 17:11:23 +0200

    change Style Attributes + SaunaImageProps #31

--- src/components/base/SaunaImage.tsx
+++ src/components/base/SaunaImage.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-// TODO : Sauna Bilder anpassen
-
-type Props ={
+export type SaunaImageProps ={
   name : string,
   imgLink : string,
   description : string,
@@ -9,24 +7,24 @@ type Props ={
   alt? : string
 }
 
-const SaunaImage = (props:Props) => {
+const SaunaImage = (props:SaunaImageProps) => {
   return (
-    <div className="space-y-25 w-70">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">{props.name}</h2>
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 xl:gap-x-8">
+    <div className="grid gap-4">
+            <h2 className="text-2xl font-extrabold text-gray-900">{props.name}</h2>
+            <div>
                 <img 
-                    src={"https://u.profitroom.pl/2018-hotel-burgblick-com/thumb/1650x600/uploads/Sauna/pool-3001209_1920.jpg"}
+                    src={props.imgLink}
                     alt={props.alt}
                 />
             </div>
-            <div className="mt-4 flex justify-between">
+            <div>
                  <div>
                    <h3 className="text-sm text-gray-700">
                      <a href={props.refLink}>
                          {"LINK"}
                      </a>
                    </h3>
-                   <p className="mt-1 text-sm text-gray-500">{props.description}</p>
+                   <p className="text-gray-500">{props.description}</p>
                  </div>
             </div>
     </div>
--- src/routes/views/app/Overview.tsx
+++ src/routes/views/app/Overview.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SaunaImage from '../../../components/base/SaunaImage'
+const imgLink1 = "https://u.profitroom.pl/2018-hotel-burgblick-com/thumb/1650x600/uploads/Sauna/pool-3001209_1920.jpg"
 
 const Overview = () => {
   return (
-        <div className='flex justify'>
-            <SaunaImage name='Karhu' imgLink='imgLink1' description='KARHU SAUNA TEXT' refLink='GoTO' />
-            <SaunaImage name='Puola' imgLink='imgLink2' description='PUOLA SAUNA TEXT' refLink='GoTO' />
-            <SaunaImage name='Telta' imgLink='imgLink3' description='TELTA SAUNA TEXT' refLink='GoTO' />
+        <div className='ml-16 mr-16 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <SaunaImage name='Karhu' imgLink={imgLink1} description='KARHU SAUNA TEXT' refLink='GoTO' />
+            <SaunaImage name='Puola' imgLink={imgLink1} description='PUOLA SAUNA TEXT' refLink='GoTO' />
+            <SaunaImage name='Telta' imgLink={imgLink1} description='TELTA SAUNA TEXT' refLink='GoTO' />
         </div>
       )
 }
