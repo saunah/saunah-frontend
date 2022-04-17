bb4817fff1985f0920c0be7470cbd4923e1fff3b
triumphvalley <timosiegenthaler@gmail.com>
2022-04-17 16:38:26 +0200

    embed in components

--- src/routes/RouteTree.tsx
+++ src/routes/RouteTree.tsx
@@ -4,20 +4,26 @@ import Home from './views/app/Home'
 import Showroom from './views/app/Showroom'
 import GreetingView from './views/app/GreetingView'
 import AuthProvider from './auth/AuthProvider'
+import AlertProvider from './alert/AlertProvider'
 import ProtectedRoute from './auth/ProtectedRoute'
 
 const RouteTree = () => {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<App />}>
-                        <Route index element={<Home />} />
-                        <Route path="/greeting/:greetingId" element={<GreetingView />} />
-                        <Route path="/showroom" element={<Showroom />} />
-                        <Route path="/protected" element={<ProtectedRoute element={<div> Protected Route </div>} />} />
-                    </Route>
-                </Routes>
+                <AlertProvider>
+                    <Routes>
+                        <Route path="/" element={<App />}>
+                            <Route index element={<Home />} />
+                            <Route path="/greeting/:greetingId" element={<GreetingView />} />
+                            <Route path="/showroom" element={<Showroom />} />
+                            <Route
+                                path="/protected"
+                                element={<ProtectedRoute element={<div> Protected Route </div>} />}
+                            />
+                        </Route>
+                    </Routes>
+                </AlertProvider>
             </AuthProvider>
         </BrowserRouter>
     )
--- src/routes/views/app/GreetingView.tsx
+++ src/routes/views/app/GreetingView.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import GreetingPrompt from '../../../components/example/GreetingPrompt'
 import { useGreeting } from '../../../hooks/greeting'
+import { useAlert } from '../../alert/AlertProvider'
 
 const GreetingView = () => {
     const { greetings, fetch } = useGreeting()
@@ -13,7 +14,9 @@ const GreetingView = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    return <GreetingPrompt greeting={greetings[0]} />
+    const { success } = useAlert()
+
+    return <GreetingPrompt greeting={greetings[0]} onClick={() => success('Sehr nicely done!')} />
 }
 
 export default GreetingView
