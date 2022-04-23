ba2e8707352b5474e86d94b01fa01b5e00f64e3b
triumphvalley <timosiegenthaler@gmail.com>
2022-04-23 13:00:02 +0200

    add basic sauna-editor

--- src/components/saunas/SaunaEditor.tsx
+++ src/components/saunas/SaunaEditor.tsx
@@ -1,12 +1,58 @@
 import { Sauna } from '../../entities/Sauna'
+import Input from '../base/Input'
 
 export type SaunaEditorProps = {
-    sauna: Sauna.Request
+    value: Sauna.Request
+    onChange?: (newValue: Sauna.Request) => void
     onSubmit?: () => void
 }
 
 const SaunaEditor = (props: SaunaEditorProps) => {
-    return <div>{`name: ${props.sauna.name}`}</div>
+    const sauna = props.value
+
+    return (
+        <div className="space-y-2">
+            <Input
+                name="Name"
+                value={sauna.name}
+                onChange={newValue => props.onChange?.({ ...sauna, name: newValue })}
+            />
+            <Input
+                name="Beschreibung"
+                value={sauna.description}
+                onChange={newValue => props.onChange?.({ ...sauna, description: newValue })}
+            />
+            <Input
+                name="Preis"
+                value={'' + sauna.price}
+                type="number"
+                onChange={newValue => props.onChange?.({ ...sauna, price: +newValue })}
+            />
+            <Input
+                name="Maximale Temperatur"
+                value={'' + sauna.maxTemp}
+                type="number"
+                onChange={newValue => props.onChange?.({ ...sauna, maxTemp: +newValue })}
+            />
+            <Input
+                name="Anzahl Personen"
+                value={'' + sauna.numberOfPeople}
+                type="number"
+                onChange={newValue => props.onChange?.({ ...sauna, numberOfPeople: +newValue })}
+            />
+            <Input
+                name="Adresse"
+                value={sauna.street}
+                onChange={newValue => props.onChange?.({ ...sauna, street: newValue })}
+            />
+            <Input name="PLZ" value={sauna.zip} onChange={newValue => props.onChange?.({ ...sauna, zip: newValue })} />
+            <Input
+                name="Ort"
+                value={sauna.location}
+                onChange={newValue => props.onChange?.({ ...sauna, location: newValue })}
+            />
+        </div>
+    )
 }
 
 export default SaunaEditor
--- src/views/routes/sauna/SaunaEditorView.tsx
+++ src/views/routes/sauna/SaunaEditorView.tsx
@@ -1,10 +1,10 @@
 import { useParams } from 'react-router-dom'
-
 import { parseId } from '../../../utils/identifiable'
 import SaunaEditor from '../../../components/saunas/SaunaEditor'
 import { useEffect, useState } from 'react'
 import api from '../../../networking/api'
 import { Sauna } from '../../../entities/Sauna'
+import { useAlert } from '../../shared/AlertProvider'
 
 const SaunaEditorView = () => {
     const params = useParams()
@@ -14,11 +14,18 @@ const SaunaEditorView = () => {
 
     useEffect(() => {
         if (saunaId) api.sauna.get(saunaId).then(sauna => setSauna(Sauna.mapToRequest(sauna)))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    const { success } = useAlert()
+    const submit = async () => {
+        if (saunaId) api.sauna.edit(saunaId, sauna).then(() => success('Die Sauna wurde erfolgreich gespeichert.'))
+        else await api.sauna.add(sauna).then(() => success('Die Sauna wurde erfolgreich erstellt.'))
+    }
 
     return (
         <div>
-            <SaunaEditor sauna={sauna} />
+            <SaunaEditor value={sauna} onChange={setSauna} onSubmit={submit} />
         </div>
     )
 }
