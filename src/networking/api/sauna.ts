0c7d6253cb6060da041efecff42fedf96a34c154
triumphvalley <timosiegenthaler@gmail.com>
2022-04-23 14:42:29 +0200

    clean up sauna api

--- src/networking/api/sauna.ts
+++ src/networking/api/sauna.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Sauna } from '../../entities/Sauna'
-import { findId } from '../../utils/identifiable'
+import { mapInArray } from '../../utils/mapping'
 import { DeepReadonly } from '../../utils/object'
 import apiRoutes from '../apiRoutes'
 
@@ -14,12 +14,12 @@ export type SaunaAPI = DeepReadonly<{
 
 const saunaApi: SaunaAPI = {
     async list(): Promise<Sauna.Response[]> {
-        // const response = await axios.get(apiRoutes.sauna.list)
-        return new Promise(r => setTimeout(() => r(saunas), 2000)) //mapInArray(response.data, Sauna.mapIn)
+        const response = await axios.get(apiRoutes.sauna.list)
+        return mapInArray(response.data, Sauna.mapIn)
     },
     async get(saunaId: number): Promise<Sauna.Response> {
-        //const response = await axios.get(apiRoutes.sauna.get(saunaId))
-        return new Promise(r => setTimeout(() => r(findId(saunas, saunaId)!), 3000)) //Sauna.mapIn(response.data)
+        const response = await axios.get(apiRoutes.sauna.get(saunaId))
+        return Sauna.mapIn(response.data)
     },
     async add(sauna: Sauna.Request): Promise<Sauna.Response> {
         const remoteRequest = Sauna.mapOut(sauna)
@@ -37,47 +37,3 @@ const saunaApi: SaunaAPI = {
 }
 
 export default saunaApi
-
-const sauna1: Sauna.Response = {
-    id: 1,
-    name: 'Sauna 1',
-    description: 'Das ist Sauna 1.',
-    price: 100000,
-    maxTemp: 100,
-    numberOfPeople: 10,
-    street: 'Hinterstrasse 12',
-    zip: '8400',
-    location: 'Winterthur',
-    type: 'Zeltsauna',
-    mobile: false,
-}
-
-const sauna2: Sauna.Response = {
-    id: 2,
-    name: 'Sauna 2',
-    description: 'Das ist Sauna 2.',
-    price: 100000,
-    maxTemp: 100,
-    numberOfPeople: 10,
-    street: 'Hinterstrasse 12',
-    zip: '8400',
-    location: 'Winterthur',
-    type: 'Zeltsauna',
-    mobile: false,
-}
-
-const sauna3: Sauna.Response = {
-    id: 3,
-    name: 'Sauna 3',
-    description: 'Das ist Sauna 3.',
-    price: 100000,
-    maxTemp: 100,
-    numberOfPeople: 10,
-    street: 'Hinterstrasse 12',
-    zip: '8400',
-    location: 'Winterthur',
-    type: 'Zeltsauna',
-    mobile: false,
-}
-
-const saunas = [sauna1, sauna2, sauna3]
