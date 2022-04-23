40109413f5b73f594a96ef1b74377234d91c83de
triumphvalley <timosiegenthaler@gmail.com>
2022-04-23 14:40:48 +0200

    add tests for sauna-editor

---
+++ src/components/saunas/SaunaEditor.test.tsx
@@ -0,0 +1,78 @@
+import { fireEvent, render, screen, within } from '@testing-library/react'
+import { Sauna } from '../../entities/Sauna'
+import SaunaEditor from './SaunaEditor'
+
+const testSauna: Sauna.Request = {
+    id: 11,
+    name: 'Test Sauna ',
+    description: 'Hoi Sauna.',
+    price: 999,
+    maxTemp: 99,
+    numberOfPeople: 9,
+    street: 'Oberstr. 12',
+    zip: '8400',
+    location: 'Winterthur',
+    type: 'Zeltsauna',
+    mobile: true,
+}
+
+const edited = {
+    name: 'Test Edited',
+    description: 'Hoi Edited',
+    price: 888,
+    maxTemp: 88,
+    numberOfPeople: 8,
+    street: 'Editstreet. 12',
+    zip: '8411',
+    location: 'Hinterthur',
+    type: 'Dachsauna',
+}
+
+const checkInputs = ['name', 'description', 'price', 'maxTemp', 'numberOfPeople', 'street', 'zip', 'location', 'type']
+
+describe('<SaunaEditor>', () => {
+    test('properties are assigned to inputs correctly', () => {
+        render(<SaunaEditor value={testSauna} />)
+
+        checkInputs.forEach(key => {
+            const input = getInputField(`input-${key}`)
+            expect(input).toBeInTheDocument()
+            expect(input).toHaveValue((testSauna as any)[key])
+        })
+
+        expect(getCheckbox('input-mobile')).toBeChecked()
+    })
+
+    test('onChange is called correctly', () => {
+        const onChange = jest.fn()
+        render(<SaunaEditor value={testSauna} onChange={onChange} />)
+
+        let calls = 0
+        Object.entries(edited).forEach(([key, value]) => {
+            fireEvent.change(getInputField(`input-${key}`), { target: { value: value } })
+            expect(onChange).toBeCalledTimes(++calls)
+            expect(onChange).toBeCalledWith({ ...testSauna, [key]: value })
+        })
+
+        fireEvent.click(getCheckbox('input-mobile'))
+        expect(onChange).toBeCalledTimes(++calls)
+        expect(onChange).toBeCalledWith({ ...testSauna, mobile: false })
+    })
+
+    test('onSubmit is called correctly', () => {
+        const onSubmit = jest.fn()
+        render(<SaunaEditor value={testSauna} onSubmit={onSubmit} />)
+        fireEvent.click(screen.getByTestId('submit-button'))
+        expect(onSubmit).toBeCalledTimes(1)
+    })
+})
+
+const getInputField = (testId: string) => {
+    const input = screen.getByTestId(testId)
+    return within(input).getByTestId('input')
+}
+
+const getCheckbox = (testId: string) => {
+    const checkbox = screen.getByTestId(testId)
+    return within(checkbox).getByTestId('checkbox')
+}
--- src/components/saunas/SaunaEditor.tsx
+++ src/components/saunas/SaunaEditor.tsx
@@ -1,5 +1,6 @@
 import { Sauna } from '../../entities/Sauna'
 import Button from '../base/Button'
+import CheckboxLabel from '../base/CheckboxLabel'
 import Input from '../base/Input'
 
 export type SaunaEditorProps = {
@@ -15,44 +16,69 @@ const SaunaEditor = (props: SaunaEditorProps) => {
         <div className="space-y-2">
             <Input
                 name="Name"
+                data-testid="input-name"
                 value={sauna.name}
                 onChange={newValue => props.onChange?.({ ...sauna, name: newValue })}
             />
             <Input
                 name="Beschreibung"
+                data-testid="input-description"
                 value={sauna.description}
                 onChange={newValue => props.onChange?.({ ...sauna, description: newValue })}
             />
             <Input
+                name="Art"
+                data-testid="input-type"
+                value={sauna.type}
+                onChange={newValue => props.onChange?.({ ...sauna, type: newValue })}
+            />
+            <Input
                 name="Preis"
+                data-testid="input-price"
                 value={'' + sauna.price}
                 type="number"
                 onChange={newValue => props.onChange?.({ ...sauna, price: +newValue })}
             />
             <Input
                 name="Maximale Temperatur"
+                data-testid="input-maxTemp"
                 value={'' + sauna.maxTemp}
                 type="number"
                 onChange={newValue => props.onChange?.({ ...sauna, maxTemp: +newValue })}
             />
             <Input
                 name="Anzahl Personen"
+                data-testid="input-numberOfPeople"
                 value={'' + sauna.numberOfPeople}
                 type="number"
                 onChange={newValue => props.onChange?.({ ...sauna, numberOfPeople: +newValue })}
             />
             <Input
                 name="Adresse"
+                data-testid="input-street"
                 value={sauna.street}
                 onChange={newValue => props.onChange?.({ ...sauna, street: newValue })}
             />
-            <Input name="PLZ" value={sauna.zip} onChange={newValue => props.onChange?.({ ...sauna, zip: newValue })} />
+            <Input
+                name="PLZ"
+                data-testid="input-zip"
+                value={sauna.zip}
+                onChange={newValue => props.onChange?.({ ...sauna, zip: newValue })}
+            />
             <Input
                 name="Ort"
+                data-testid="input-location"
                 value={sauna.location}
                 onChange={newValue => props.onChange?.({ ...sauna, location: newValue })}
             />
-            <Button title="Speichern" onClick={props.onSubmit} />
+            <CheckboxLabel
+                data-testid="input-mobile"
+                label="Mobil"
+                details="Die Sauna ist mobil und kann abgeholt oder geliefert werden."
+                value={sauna.mobile}
+                onChange={newValue => props.onChange?.({ ...sauna, mobile: newValue })}
+            />
+            <Button title="Speichern" data-testid="submit-button" onClick={props.onSubmit} />
         </div>
     )
 }
