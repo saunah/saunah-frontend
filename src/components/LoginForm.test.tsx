f3541b85c72c8992798f67c5b9d89e82f893f752
inderdav <inderdav@students.zhaw.ch>
2022-04-28 08:00:25 +0200

    init LoginForm and test

---
+++ src/components/LoginForm.test.tsx
@@ -0,0 +1,3 @@
+import { render, screen, within, fireEvent } from '@testing-library/react'
+import LoginForm from './LoginForm'
+import { User } from '../entities/User' 
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,44 @@
+import Button from './base/Button'
+import { Link } from 'react-router-dom'
+import Input from './base/Input'
+import { User } from '../entities/User'
+
+export type LoginFormProps = {
+    user: User.Edit
+    onChange?: (user: User.Edit) => void
+}
+
+const LoginForm = (props: LoginFormProps) => {
+    return (
+        <div className="grid gap-4">
+            <div className="ml-10 mr-10 grid gap-x-6 gap-y-2 grid-cols-1">
+                <Input
+                    name="Username"
+                    placeholder="Username"
+                    value={"props.user.username"}
+                    // Musste rausgenommen werden, username auf User.Edit nicht mehr vorhanden
+                    //onChange={username => props.onChange?.({ ...props.user, usernamePlaceholder })}
+                />
+                <Input
+                    name="Password"
+                    placeholder="Password"
+                    value={props.user.password}
+                    onChange={password => props.onChange?.({ ...props.user, password })}
+                />
+            </div>
+            <div className="ml-20 mr-20 grid gap-x-8 gap-y-3 grid-cols-2">
+                <Button onClick={() => console.log('Login')}> Login </Button>
+                <Link
+                    to="/" //change when we know where the link needs to go
+                    className={`inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker`}
+                >
+                    Forgot Password?
+                </Link>
+            </div>
+            username: {"props.user.username"} <br />
+            password: {props.user.password}
+        </div>
+    )
+}
+
+export default LoginForm
