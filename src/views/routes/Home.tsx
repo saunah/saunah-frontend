8e09ad8f087e36468532dc1dff26042a61fd0881
triumphvalley <timosiegenthaler@gmail.com>
2022-05-01 14:37:28 +0200

    restore home

--- src/views/routes/Home.tsx
+++ src/views/routes/Home.tsx
@@ -1,25 +1,10 @@
 import PageTitle from '../../components/base/PageTitle'
-import ImageDropzone from '../../components/base/ImageDropzone'
-import { useState } from 'react'
-import axios from 'axios'
-import Button from '../../components/base/Button'
 
 const Home = () => {
-    const [files, setFiles] = useState<File[]>([])
-
-    const onSubmit = () => {
-        const multiplart = new FormData()
-        files.forEach((file, index) => multiplart.append('images', file))
-        axios.post('/sauna/1/addImage', multiplart).then(() => console.log('yeah'))
-    }
-
     return (
-        <>
+        <div>
             <PageTitle> You are home. </PageTitle>
-            <ImageDropzone onFilesChanged={setFiles} />
-            <Button onClick={onSubmit}>Hochladen</Button>
-
-            {/* <p>
+            <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
                 labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
                 ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
@@ -92,8 +77,8 @@ const Home = () => {
                 vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
                 accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te
                 feugait nulla facilisi. Lorem ipsum dolor sit amet,
-            </p> */}
-        </>
+            </p>
+        </div>
     )
 }
 
