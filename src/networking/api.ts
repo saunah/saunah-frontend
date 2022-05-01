5c11d7241f94ff81ad0a66fa62553439c9f18f98
triumphvalley <timosiegenthaler@gmail.com>
2022-05-01 12:55:52 +0200

    add new api to global api

--- src/networking/api.ts
+++ src/networking/api.ts
@@ -1,13 +1,20 @@
 import sauna, { SaunaAPI } from './api/sauna'
+import saunaImages, { SaunaImageAPI } from './api/saunaImages'
 import { readonly } from '../utils/object'
 
 const api = {
     sauna,
+    saunaImages,
 }
 
 export default readonly(api)
 
 export function mockSaunaAPI<T extends SaunaAPI>(mockApi: T): T {
     api.sauna = mockApi
+    return mockApi
+}
+
+export function mockSaunaImageAPI<T extends SaunaImageAPI>(mockApi: T): T {
+    api.saunaImages = mockApi
     return mockApi
 }
