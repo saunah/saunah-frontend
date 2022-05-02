b2d61b7497cc40a2dacfa7d4a7b330ff95e68278
inderdav <inderdav@students.zhaw.ch>
2022-05-02 19:19:48 +0200

    init test Overview

--- src/views/routes/Overview.test.tsx
+++ src/views/routes/Overview.test.tsx
@@ -1,17 +1,54 @@
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
+import { ReactNode } from 'react'
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom'
+import { Sauna } from '../../entities/Sauna'
+import { mockSaunaAPI } from '../../networking/api'
 import Overview from './Overview'
 
-describe('<Overview>', () => {
-    test('render correctly', () => {
-        render(
-            <BrowserRouter>
-                <Overview />
-            </BrowserRouter>
-        )
-        const Ov1 = screen.getByTestId('overviewTID')
-        expect(Ov1).toHaveClass('ml-16 mr-16 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3')
-        // Sinvoll wenn definitiver TextContent steht
-        //expect(Ov1).toHaveTextContent("")
+const defaultMock = () => {
+    return {
+        list: jest.fn(() => Promise.resolve([sauna1])),
+        get: jest.fn(() => Promise.resolve(sauna1)),
+        add: jest.fn(() => Promise.resolve(sauna1)),
+        edit: jest.fn(() => Promise.resolve(sauna1)),
+        remove: jest.fn(() => Promise.resolve()),
+    }
+}
+
+const wrapper = (startRoute: string) => (props: { children?: ReactNode }) => {
+    return(
+        <MemoryRouter initialEntries={[startRoute]}>
+            <Routes>
+                <Route path="/sauna/:saunaId/list" element={props.children} />
+            </Routes>
+        </MemoryRouter>    
+    )
+}
+
+const sauna1: Sauna.Response = {
+    id: 1,
+    name: 'Sauna 1',
+    description: 'Das ist Sauna 1.',
+    price: 100000,
+    maxTemp: 100,
+    numberOfPeople: 10,
+    street: 'Hinterstrasse 12',
+    zip: 8400,
+    location: 'Winterthur',
+    type: 'Zeltsauna',
+    mobile: false,
+}
+
+const waitForStateUpdate = () => screen.findByTestId('overviewTID')
+
+describe('<Overview Tests>', () => {
+    test('the correct sauna is fetched on the edit page', async () => {
+        const mock = mockSaunaAPI(defaultMock())
+
+        render(<Overview />, { wrapper: wrapper('/saunas/') })
+        await waitForStateUpdate()
+        expect(screen.getByTestId('overviewTID')).toBeInTheDocument()
+
+        expect(mock.list).toBeCalledTimes(1)
     })
 })
