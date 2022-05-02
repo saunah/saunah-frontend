b67ac8beb03cbc99c714e5714c4aa5c16da34d8c
triumphvalley <timosiegenthaler@gmail.com>
2022-05-02 14:59:27 +0200

    improve styling of image-carousel

--- src/components/saunas/SaunaImageCarousel.tsx
+++ src/components/saunas/SaunaImageCarousel.tsx
@@ -3,6 +3,9 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { SaunaImage } from '../../entities/SaunaImage'
 import apiRoutes, { getAbsoluteUrl } from '../../networking/apiRoutes'
+import { useRef } from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import './SaunaImageCarousel.css'
 
 export type SaunaImageCarouselProps = {
     images: SaunaImage.Response[]
@@ -10,24 +13,39 @@ export type SaunaImageCarouselProps = {
 
 const SaunaImageCarousel = (props: SaunaImageCarouselProps) => {
     const settings: Settings = {
-        dots: false,
+        dots: true,
         infinite: true,
         speed: 500,
+        arrows: false,
     }
 
+    const ref = useRef<Slider>(null)
+
     return (
-        <div className="px-12  bg-primary-200">
-            <Slider {...settings} className="">
-                {props.images.map(image => (
-                    <div key={image.fileName}>
-                        <img
-                            src={getAbsoluteUrl(apiRoutes.saunaImages.get(image.fileName))}
-                            alt={image.fileName}
-                            className="h-96 m-auto"
-                        />
-                    </div>
-                ))}
-            </Slider>
+        <div className="relative rounded-lg overflow-hidden">
+            <button className="carousel-side-button top-0 left-0" onClick={() => ref.current?.slickPrev()}>
+                <ChevronLeftIcon />
+            </button>
+            {/* The desired padding of the images is 6. px-14 is 8 (side-button) + 6. pb-14 because of dots.*/}
+            <div className="px-14 pt-6 pb-12 bg-primary-100">
+                <Slider {...settings} ref={ref}>
+                    {props.images.map(image => (
+                        <div key={image.fileName}>
+                            <div
+                                className="h-60 md:h-80 lg:h-96 bg-contain bg-no-repeat bg-center"
+                                style={{
+                                    backgroundImage: `url(${getAbsoluteUrl(
+                                        apiRoutes.saunaImages.get(image.fileName)
+                                    )})`,
+                                }}
+                            />
+                        </div>
+                    ))}
+                </Slider>
+            </div>
+            <button className="carousel-side-button top-0 right-0" onClick={() => ref.current?.slickNext()}>
+                <ChevronRightIcon />
+            </button>
         </div>
     )
 }
--- src/views/routes/Home.tsx
+++ src/views/routes/Home.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react'
 import PageTitle from '../../components/base/PageTitle'
+import SaunaImageCarousel from '../../components/saunas/SaunaImageCarousel'
+import { SaunaImage } from '../../entities/SaunaImage'
+import api from '../../networking/api'
 
 const Home = () => {
+    const [images, setImages] = useState<SaunaImage.Response[]>([])
+
+    useEffect(() => {
+        api.saunaImages.list(1).then(setImages)
+    }, [])
+
     return (
         <div>
             <PageTitle> You are home. </PageTitle>
-            <p>
+            <SaunaImageCarousel images={images} />
+            {/* <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
                 labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
                 ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
@@ -77,7 +88,7 @@ const Home = () => {
                 vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
                 accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te
                 feugait nulla facilisi. Lorem ipsum dolor sit amet,
-            </p>
+            </p> */}
         </div>
     )
 }
