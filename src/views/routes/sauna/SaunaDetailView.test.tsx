dd457e6f4a0d38c35a23a2448e24fcbe16a5d920
triumphvalley <timosiegenthaler@gmail.com>
2022-05-04 14:11:14 +0200

    fix tests

--- src/views/routes/sauna/SaunaDetailView.test.tsx
+++ src/views/routes/sauna/SaunaDetailView.test.tsx
@@ -1,12 +1,39 @@
 import { render, screen } from '@testing-library/react'
 import { ReactNode } from 'react'
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { Sauna } from '../../../entities/Sauna'
+import { mockSaunaAPI } from '../../../networking/api'
 import SaunaDetailView from './SaunaDetailView'
 
+const defaultMock = () => {
+    return {
+        list: jest.fn(() => Promise.resolve([sauna1])),
+        get: jest.fn(() => Promise.resolve(sauna1)),
+        add: jest.fn(() => Promise.resolve(sauna1)),
+        edit: jest.fn(() => Promise.resolve(sauna1)),
+        remove: jest.fn(() => Promise.resolve()),
+    }
+}
+
+const sauna1: Sauna.Response = {
+    id: 1,
+    name: 'Sauna 1',
+    description: 'Das ist Sauna 1.',
+    price: 100000,
+    maxTemp: 100,
+    numberOfPeople: 10,
+    street: 'Hinterstrasse 12',
+    zip: 8400,
+    location: 'Winterthur',
+    type: 'Zeltsauna',
+    mobile: false,
+}
+
 describe('<SaunaDetailView>', () => {
-    test('is here', () => {
+    test('is here', async () => {
+        mockSaunaAPI(defaultMock())
         render(<SaunaDetailView />, { wrapper })
-        expect(screen.getByText('Details', { exact: false })).toBeInTheDocument()
+        expect(await screen.findByText('Details', { exact: false })).toBeInTheDocument()
     })
 })
 
