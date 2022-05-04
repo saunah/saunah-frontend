c5d72ce2ddea94d4349f3df0932a36ad49f040d3
triumphvalley <timosiegenthaler@gmail.com>
2022-05-04 12:57:37 +0200

    improve SaunaCard

--- src/components/saunas/SaunaCard.tsx
+++ src/components/saunas/SaunaCard.tsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Sauna } from '../../entities/Sauna'
+import { SaunaImage } from '../../entities/SaunaImage'
+import api from '../../networking/api'
+import apiRoutes, { getAbsoluteUrl } from '../../networking/apiRoutes'
+import ButtonLink from '../base/ButtonLink'
 
 export type SaunaCardProps = {
     sauna: Sauna.Response
 }
 
-const PLACEHOLDER_LINK =
-    'https://u.profitroom.pl/2018-hotel-burgblick-com/thumb/1650x600/uploads/Sauna/pool-3001209_1920.jpg'
-
 const SaunaCard = (props: SaunaCardProps) => {
+    const [images, setImages] = useState<SaunaImage.Response[]>([])
+    const imgLink = images[0] ? getAbsoluteUrl(apiRoutes.saunaImages.get(images[0].fileName)) : ''
+    const navLink = `/saunas/${props.sauna.id}`
+
+    useEffect(() => {
+        api.saunaImages.list(props.sauna.id).then(setImages)
+    }, [props.sauna])
+
     return (
-        <div className="grid gap-4">
-            <h2 className="text-2xl font-extrabold text-gray-900">{props.sauna.name}</h2>
-            <div>
-                <img src={PLACEHOLDER_LINK /* TODO: add correct link */} alt={'props.alt'} />
-            </div>
-            <div>
-                <Link to="/">LINK</Link>
-                <p className="text-gray-500">{props.sauna.description}</p>
+        <div>
+            <Link to={navLink}>
+                <div
+                    className="aspect-video bg-center bg-no-repeat bg-cover rounded-2xl shadow-xl shadow-primary-900/[0.1] origin-center hover:scale-[1.02] transition duration-100"
+                    style={{ backgroundImage: `url(${imgLink})` }}
+                />
+            </Link>
+            <div className="pt-6 px-4">
+                <h2 className="text-3xl font-bold text-primary-600 mb-2">{props.sauna.name}</h2>
+                <p className="text-primary-900">{props.sauna.description}</p>
+
+                <ButtonLink className="mt-4" to={navLink}>
+                    Sauna ansehen
+                </ButtonLink>
             </div>
         </div>
     )
