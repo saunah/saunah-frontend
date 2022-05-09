5ff34cd996c7561419d2b00c9b7b093b5a9f2866
stutzali <alinestutz@gmx.ch>
2022-05-09 14:29:32 +0200

    added more details

--- src/components/saunas/SaunaDetail.tsx
+++ src/components/saunas/SaunaDetail.tsx
@@ -8,7 +8,7 @@ export type SaunaDetailProps = {
 
 const SaunaDetail = (props: SaunaDetailProps) => {
     return (
-        <div className="grid gap-4" data-testid={props['data-testid'] ||'sauna-detail'}>
+        <div className="grid gap-4" data-testid={props['data-testid'] || 'sauna-detail'}>
             <h2 data-testid="title" className="text-2xl font-extrabold text-gray-900">
                 {props.sauna.name}
             </h2>
@@ -16,6 +16,24 @@ const SaunaDetail = (props: SaunaDetailProps) => {
                 <p data-testid="description" className="text-gray-500">
                     {props.sauna.description}
                 </p>
+            </div>
+            <div className="text-gray-500 grid gap-8 grid-cols-2">
+                <div>
+                    <div>Preis:</div>
+                    <div>Maximale Temperatur in Celsius:</div>
+                    <div>Anzahl Personen:</div>
+                    <div>Hauptstandort:</div>
+                    <div>Art der Sauna:</div>
+                </div>
+                <div>
+                    <div>{props.sauna.price}</div>
+                    <div>{props.sauna.maxTemp}</div>
+                    <div>{props.sauna.numberOfPeople}</div>
+                    <div>
+                        {props.sauna.zip} {props.sauna.location}
+                    </div>
+                    <div>{props.sauna.type}</div>
+                </div>
             </div>
         </div>
     )
