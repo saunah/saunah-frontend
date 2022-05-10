02ba11960f032bb9ae61bdfb16bf82a3d34af2f2
inderdav <inderdav@students.zhaw.ch>
2022-05-10 15:56:03 +0200

    init new type + Form

---
+++ src/components/SetNewPasswordForm.tsx
@@ -0,0 +1,54 @@
+import Button from './base/Button'
+import Input from './base/Input'
+import { SetNewPassword } from '../entities/SetNewPassword'
+
+// eslint-disable-next-line
+export type SetNewPassword = {
+    request: SetNewPassword.Request
+    onChange?: (request: SetNewPassword.Request) => void
+    onSubmit?: () => void
+}
+
+const SetNewPasswordForm = (props: SetNewPassword) => {
+    return (
+        <div>
+            <div className="space-y-2">
+                <Input
+                    data-testid="mailaddress"
+                    name="Mailadresse"
+                    placeholder="Mailadresse eingeben"
+                    autoComplete="mailaddress"
+                    value={props.request.mailadress}
+                    onChange={mailadress => props.onChange?.({ ...props.request, mailadress })}
+                />
+            </div>
+            <div className="space-y-2">
+                <Input
+                    data-testid="set-new-pw-field"
+                    name="neues Passwort"
+                    placeholder="neues Passwort eingeben"
+                    autoComplete="password"
+                    value={props.request.newPassword}
+                    onChange={newPassword => props.onChange?.({ ...props.request, newPassword })}
+                />
+            </div>
+            <div className="space-y-2">
+                <Input
+                    data-testid="confirm-new-pw-field"
+                    name="passwort bestätigen"
+                    placeholder="Passwort bestätigen"
+                    autoComplete="password"
+                    value={props.request.newPassword}
+                    onChange={newPassword => props.onChange?.({ ...props.request, newPassword })}
+                />
+            </div>
+            <div className="flex justify-between">
+                <Button className="mt-6" data-testid="send-button" onClick={props.onSubmit}>
+                    Passwort Reset
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default SetNewPasswordForm
