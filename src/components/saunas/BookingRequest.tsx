b59bcdfdb7b8f99391b83db918e3e66f31599a07
stutzali <alinestutz@gmx.ch>
2022-05-11 16:44:05 +0200

    draft for booking

--- src/components/saunas/BookingRequest.tsx
+++ src/components/saunas/BookingRequest.tsx
@@ -1,6 +1,7 @@
 import { Booking } from '../../entities/Booking'
 import Button from '../base/Button'
 import Checkbox from '../base/Checkbox'
+import CheckboxLabel from '../base/CheckboxLabel'
 import Dropdown from '../base/Dropdown'
 import Input from '../base/Input'
 
@@ -15,8 +16,43 @@ const BookingRequest = (props: BookingRequestProps) => {
 
     return (
         <div data-testid="booking-request">
-            <div className="space-y-2">
-                <Checkbox value={false} disabled={false} />
+            <div className="grid gap-4">
+                <Input
+                    name="Wunschort der Sauna"
+                    value={booking.location}
+                    onChange={newValue => props.onChange?.({ ...booking, location: newValue })}
+                />
+                <div className="grid gap-4 grid-cols-2">
+                    <Checkbox //calender?
+                        value={booking.washService}
+                        onChange={newValue => props.onChange?.({ ...booking, washService: newValue })}
+                    />
+                    <span>Reinigung der Sauna 50CHF</span>
+
+                    <Checkbox
+                        value={booking.deposit}
+                        onChange={newValue => props.onChange?.({ ...booking, deposit: newValue })}
+                    />
+                    <span>Kaution</span>
+                    <Checkbox
+                        value={booking.handTowel}
+                        onChange={newValue => props.onChange?.({ ...booking, deposit: newValue })}
+                    />
+                    <Input
+                        name="Handtuch"
+                        value={'' + booking.handTowel}
+                        type="number"
+                        //onChange={newValue => props.onChange?.({ ...booking, handTowel: newValue })}
+                    />
+                </div>
+
+                <CheckboxLabel
+                    label="Kaution"
+                    details="100 CHF"
+                    value={booking.deposit}
+                    onChange={newValue => props.onChange?.({ ...booking, deposit: newValue })}
+                    disabled={true}
+                />
             </div>
             <Button className="mt-6" title="Buchen" data-testid="submit-button" onClick={props.onSubmit} />
         </div>
