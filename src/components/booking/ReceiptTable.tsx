1c996aaf62c21f2ef1f1c220acb2390972f14dc8
triumphvalley <timosiegenthaler@gmail.com>
2022-05-13 12:26:10 +0200

    cleanup receipt-table

--- src/components/booking/ReceiptTable.tsx
+++ src/components/booking/ReceiptTable.tsx
@@ -6,70 +6,68 @@ export type ReceiptProps = {
 }
 
 const ReceiptTable = (props: ReceiptProps) => {
-    const receipt = props.receipt
-    return (
-        <div>
-            <Table
-                headings={['Kostenpunkt', 'Preis', 'Gebucht', 'Total']}
-                elements={[
-                    [
-                        'Stundenpreis',
-                        `CHF ${receipt.prices.hourlyRate} / h`,
-                        `${receipt.booked.duration} h`,
-                        `CHF ${receipt.prices.hourlyRate * receipt.booked.duration}`,
-                    ],
-                    [
-                        'Transportservice',
-                        `CHF ${receipt.prices.transportService} / km`,
-                        `${receipt.booked.transportService} km`,
-                        `CHF ${receipt.prices.transportService * receipt.booked.transportService}`,
-                    ],
-                    [
-                        'Handtücher',
-                        `CHF ${receipt.prices.handTowel} / Stück`,
-                        `${receipt.booked.handTowel} Stück`,
-                        `CHF ${receipt.prices.handTowel * receipt.booked.handTowel}`,
-                    ],
-                    [
-                        'Holzkisten',
-                        `CHF ${receipt.prices.wood} / Stück`,
-                        `${receipt.booked.wood} Stück`,
-                        `CHF ${receipt.prices.wood * receipt.booked.wood}`,
-                    ],
-                    [
-                        'Saunawichtel',
-                        `CHF ${receipt.prices.saunahImp} / h`,
-                        `${receipt.booked.saunahImp} h`,
-                        `CHF ${receipt.prices.saunahImp * receipt.booked.saunahImp}`,
-                    ],
-                    [
-                        'Waschservice',
-                        `CHF ${receipt.prices.washService}`,
-                        `${receipt.booked.washService ? 'Gebucht' : '-'}`,
-                        `CHF ${receipt.booked.washService ? receipt.prices.washService : 0}`,
-                    ],
-                    [
-                        'Depot',
-                        `CHF ${receipt.prices.deposit}`,
-                        `${receipt.booked.deposit ? '' : '-'}`,
-                        `CHF ${receipt.booked.deposit ? receipt.prices.deposit : 0}`,
-                    ],
-                    ['Total', '', '', `CHF ${Receipt.calculateTotal(receipt)}`],
-                ]}
-            />
+    return <Table headings={['Kostenpunkt', 'Preis', 'Gebucht', 'Total']} elements={buildElements(props.receipt)} />
+}
 
-            {/* <div className="p-4 rounded-lg bg-primary-100 grid grid-cols-4">
-                <div> Stundenpreis der Sauna </div>
-                <div> CHF 4 / h </div>
-                <div> Hello </div>
-                <div> Hello </div>
-                <div> Hello </div>
-                <div> Hello </div>
-                <div> Hello </div>
-                <div> Hello </div>
-            </div> */}
-        </div>
-    )
+function buildElements(receipt: Receipt.Response) {
+    const elements = [
+        [
+            'Stundenpreis',
+            `CHF ${receipt.prices.hourlyRate} / h`,
+            `${receipt.booked.duration} h`,
+            `CHF ${receipt.prices.hourlyRate * receipt.booked.duration}`,
+        ],
+    ]
+
+    if (receipt.booked.transportService > 0)
+        elements.push([
+            'Transportservice',
+            `CHF ${receipt.prices.transportService} / km`,
+            `${receipt.booked.transportService} km`,
+            `CHF ${receipt.prices.transportService * receipt.booked.transportService}`,
+        ])
+
+    if (receipt.booked.handTowel > 0)
+        elements.push([
+            'Handtücher',
+            `CHF ${receipt.prices.handTowel} / Stück`,
+            `${receipt.booked.handTowel} Stück`,
+            `CHF ${receipt.prices.handTowel * receipt.booked.handTowel}`,
+        ])
+
+    if (receipt.booked.wood > 0)
+        elements.push([
+            'Holzkisten',
+            `CHF ${receipt.prices.wood} / Stück`,
+            `${receipt.booked.wood} Stück`,
+            `CHF ${receipt.prices.wood * receipt.booked.wood}`,
+        ])
+    if (receipt.booked.saunahImp > 0)
+        elements.push([
+            'Saunawichtel',
+            `CHF ${receipt.prices.saunahImp} / h`,
+            `${receipt.booked.saunahImp} h`,
+            `CHF ${receipt.prices.saunahImp * receipt.booked.saunahImp}`,
+        ])
+
+    if (receipt.booked.washService)
+        elements.push([
+            'Waschservice',
+            `CHF ${receipt.prices.washService}`,
+            `${receipt.booked.washService ? 'Gebucht' : '-'}`,
+            `CHF ${receipt.booked.washService ? receipt.prices.washService : 0}`,
+        ])
+
+    if (receipt.booked.deposit)
+        elements.push([
+            'Depot',
+            `CHF ${receipt.prices.deposit}`,
+            `${receipt.booked.deposit ? '' : '-'}`,
+            `CHF ${receipt.booked.deposit ? receipt.prices.deposit : 0}`,
+        ])
+
+    elements.push(['Total', '', '', `CHF ${Receipt.calculateTotal(receipt)}`])
+    return elements
 }
 
 export default ReceiptTable
