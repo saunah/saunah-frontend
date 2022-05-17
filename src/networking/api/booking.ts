a20d45c1731542170d57e9c97c7cefde2c03ba98
triumphvalley <timosiegenthaler@gmail.com>
2022-05-18 00:09:10 +0200

    remove commented out code

--- src/networking/api/booking.ts
+++ src/networking/api/booking.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 import apiRoutes from '../apiRoutes'
 import { mapInArray } from '../../utils/mapping'
 import { Booking } from '../../entities/Booking'
-import { BookingMock } from './booking.mock'
 
 export type BookingAPI = DeepReadonly<{
     listAll(): Promise<Booking.Response[]>
@@ -19,37 +18,30 @@ const bookingApi: BookingAPI = {
     async listAll(): Promise<Booking.Response[]> {
         const response = await axios.get(apiRoutes.booking.listAll)
         return mapInArray(response.data, Booking.mapIn)
-        // return Promise.resolve([BookingMock.getSampleResponse()])
     },
     async list(): Promise<Booking.Response[]> {
         const response = await axios.get(apiRoutes.booking.list)
         return mapInArray(response.data, Booking.mapIn)
-        // return Promise.resolve([BookingMock.getSampleResponse()])
     },
     async get(bookingId: number): Promise<Booking.Response> {
         const response = await axios.get(apiRoutes.booking.get(bookingId))
         return Booking.mapIn(response.data)
-        // return Promise.resolve(BookingMock.getSampleResponse())
     },
     async add(booking: Booking.Request): Promise<Booking.Response> {
         const remoteRequest = Booking.mapOut(booking)
         const response = await axios.post(apiRoutes.booking.add, remoteRequest)
         return Booking.mapIn(response.data)
-        // return Promise.resolve(BookingMock.getSampleResponse())
     },
     async edit(bookingId: number, booking: Booking.Request): Promise<Booking.Response> {
         const remoteRequest = Booking.mapOut(booking)
         const response = await axios.put(apiRoutes.booking.edit(bookingId), remoteRequest)
         return Booking.mapIn(response.data)
-        // return Promise.resolve(BookingMock.getSampleResponse())
     },
     async approve(bookingId: number): Promise<void> {
         await axios.put(apiRoutes.booking.approve(bookingId))
-        // return Promise.resolve()
     },
     async cancel(bookingId: number): Promise<void> {
         await axios.put(apiRoutes.booking.cancel(bookingId))
-        // return Promise.resolve()
     },
 }
 
